4d64be64af768dfff171b7bffbedcd99e5fe70ae
MaximeDAM <m.dalleave@codeur.online>
2022-02-01 10:24:08 +0000

    amelioration bouton retour

--- js/index.js
+++ js/index.js
@@ -1,13 +1,12 @@
+let mybutton = document.getElementById("backTop");
 
 window.onscroll = function () {
-let mybutton = document.getElementById("to_Top");
-  
-  if (document.body.scrollTop > 280 || 
-    document.documentElement.scrollTop > 280) {
-     console.log("tag1");
-      mybutton.className = "active";
-    console.log(mybutton.className);
+  if (document.body.scrollTop > 280 || document.documentElement.scrollTop > 280) {
+    
+    mybutton.className = " back-to-top__active";
+
   } else {
-    mybutton.className = "inactive";
+    mybutton.className = " back-to-top__inactive";
+    mybutton.clas
   }
 };
